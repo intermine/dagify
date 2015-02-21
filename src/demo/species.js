518d95e988820093d99ce66919ad1cb15d7a90aa
Alex Kalderimis <alexkalderimis@gmail.com>
2015-02-21 17:07:06 +0000

    Pass methods to the constructor.

--- src/demo/species.js
+++ src/demo/species.js
@@ -3,18 +3,60 @@
 var Widget = require('../widget');
 var ControlsView = require('../views/controls');
 var graph = require('./fixtures/species');
+var opts = {
+  rankdir: 'tb',
+  rootName: 'evolutionary tree',
+  currentRoot: null,
+  roots: []
+};
 
-function load (container, summary, controls) {
-  var widget = new Widget(graph);
+var SpeciesWidget = {
+  getNodeLabel: function (node) {
+    if (node.status === 'common_ancestor') {
+      return '';
+    }
+    return node.name;
+  },
+  getNodeID: function (node) {
+    return node.name;
+  },
+  getBaseNode: function (node) {
+    if (node.status === 'common_ancestor') {
+      return {rx: 2, ry: 2, padding: 2};
+    } else {
+      var n = {rx: 2, ry: 2};
+      if (node.status === 'extant') {
+        n.rank = 'same';
+      }
+      return n;
+    }
+  },
+  getBaseEdge: function (edge) {
+    if (edge.ancestor.status === 'common_ancestor') {
+      return {arrowhead: 'none'};
+    }
+    return {arrowhead: 'vee'};
+  },
+  getEdgeSource: function (edge) {
+    return edge.subject.name;
+  },
+  getEdgeTarget: function (edge) {
+    return edge.ancestor.name;
+  },
+  getEdgeLabel: function (edge) {
+    if (edge.ancestor.status === 'common_ancestor') {
+      return '';
+    }
+    return 'descends from';
+  }
+};
+
+module.exports = function load (container, summary, controls) {
+  var data = {opts: opts,nodes: graph.nodes,edges: graph.edges};
+  var widget = new Widget(data, SpeciesWidget);
   widget.graphElement = container;
   widget.summaryElement = summary;
 
-  widget.graphState.set({
-    rankdir: 'tb',
-    rootName: 'evolutionary tree',
-    currentRoot: null,
-    roots: []
-  });
   widget.on('change:graph', function (graph) {
     var currentRoot = graph.graph().currentRoot;
     if (!currentRoot) { // Only want to know about whole graph.
@@ -35,45 +77,5 @@ function load (container, summary, controls) {
   });
   graphControls.render();
 
-  widget.getNodeLabel = function (node) {
-    if (node.status === 'common_ancestor') {
-      return '';
-    }
-    return node.name;
-  };
-  widget.getNodeID = function (node) {
-    return node.name;
-  };
-  widget.getBaseNode = function (node) {
-    if (node.status === 'common_ancestor') {
-      return {rx: 2, ry: 2, padding: 2};
-    } else {
-      var n = {rx: 2, ry: 2};
-      if (node.status === 'extant') {
-        n.rank = 'same';
-      }
-      return n;
-    }
-  };
-  widget.getBaseEdge = function (edge) {
-    if (edge.ancestor.status === 'common_ancestor') {
-      return {arrowhead: 'none'};
-    }
-    return {arrowhead: 'vee'};
-  };
-  widget.getEdgeSource = function (edge) {
-    return edge.subject.name;
-  };
-  widget.getEdgeTarget = function (edge) {
-    return edge.ancestor.name;
-  };
-  widget.getEdgeLabel = function (edge) {
-    if (edge.ancestor.status === 'common_ancestor') {
-      return '';
-    }
-    return 'descends from';
-  };
   widget.render();
-}
-
-module.exports = load;
+};
--- src/demo/words.js
+++ src/demo/words.js
@@ -2,27 +2,29 @@
 
 var Widget = require('../widget');
 var graph = require('./fixtures/small');
+var methods = {
+  getNodeLabel: function (node) {
+    return node.name;
+  },
+  getNodeID: function (node) {
+    return node.identifier;
+  },
+  getEdgeSource: function (edge) {
+    return edge.child;
+  },
+  getEdgeTarget: function (edge) {
+    return edge.parent;
+  },
+  getEdgeLabel: function (edge) {
+    return edge.relationship;
+  }
+}
 
 function load (container, summary) {
-  var widget = new Widget(graph);
+  var widget = new Widget(graph, methods);
   widget.graphElement = container;
   widget.summaryElement = summary;
 
-  widget.getNodeLabel = function (node) {
-    return node.name;
-  };
-  widget.getNodeID = function (node) {
-    return node.identifier;
-  };
-  widget.getEdgeSource = function (edge) {
-    return edge.child;
-  };
-  widget.getEdgeTarget = function (edge) {
-    return edge.parent;
-  };
-  widget.getEdgeLabel = function (edge) {
-    return edge.relationship;
-  };
   widget.render();
 }
 
